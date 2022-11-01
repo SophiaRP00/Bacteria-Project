5bf23eb4095c16669a24788fd9804cb78f1683e9
Nicklas <s224218@dtu.dk>
2022-11-01 12:32:35 +0100

    Added template to write our code - Good Luck BTW

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+def dataLoad(filename):
+    # Insert your code here
+
+def dataStatistics(data, statistics):
+    # Insert your code here
+
+def dataPlot(data):
+    # Insert your code here
+
+def main():
+    # Insert your code here
