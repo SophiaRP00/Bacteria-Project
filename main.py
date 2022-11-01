da5f5076db7219ad24184790532e5f7649d57d50
Nicklas <s224218@dtu.dk>
2022-11-01 18:40:09 +0100

    Modified main, created test doc

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
-
 def dataLoad(filename):
     # Insert your code here
+    return
 
 def dataStatistics(data, statistics):
     # Insert your code here
+    return
 
 def dataPlot(data):
     # Insert your code here
+    return
 
 def main():
     # Insert your code here
+    return
