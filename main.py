7299c3347434d186a70cbb7cad1593da2d3912b1
Nicklas <s224218@dtu.dk>
2022-11-03 13:31:58 +0100

    dataLoad done, needs comments tho

--- main.py
+++ main.py
@@ -3,42 +3,30 @@ import numpy as np
 credits = "created by Nicklas: s224218, Sophia: s224222, Jonas: s22####"
 
 def dataLoad(filename):
-    ##########################
-    ## Loads data from file ##
-    filein = open(filename, "r")
-    lines = filein.readlines()
-    normalText = "".join(lines)
-    ##########################
-
-    #Converting file to Matrix
-
-    # for row in filename:
-    #     data = []
-    #     data.append([int(x) for x in row.split()])
-
+    ####################################
+    ###  Converting file to Matrix   ###
+    ### By Jonas, Nicklas and Sophia ###   
+    ####################################
     tmp = np.loadtxt(filename, dtype=float)
     data = np.zeros(3)
-    print(data)
     for n in range(tmp[:,0].size):
         stack = False
-        if tmp[n,0] > 10 and tmp[n,0] < 60 and tmp[n,1] > 0 and tmp[n,2] >= 1 and tmp[n,2] <= 4:
+        if tmp[n,0] > 10 and tmp[n,0] < 60:
             stack = True
-            #data = np.vstack((data, tmp[n,:]))
         else:
-            print("Invalid data on line " + str(n + 1) + ": Not in range between 10-60 ;_;"
-
+            print("Invalid data on line " + str(n + 1) + ": Not in range between 10-60 (ಠ_ಠ)\n")
         if tmp[n,1] > 0:
             stack = True
         else:
-            print("Invalid data on line " + str(n + 1) + ": Not a positive number :(")
-
+            print("Invalid data on line " + str(n + 1) + ": Not a positive number (¬_¬)\n")
+        if tmp[n,2] >= 1 and tmp[n,2] <= 4:
+            stack = True
+        else:
+            print("Invalid data on line " + str(n + 1) + ": Not a valid bacteria (╯°□°）╯︵ ┻━┻\n")
+        if stack == True:
+            data = np.vstack((data, tmp[n,:]))
     data = np.delete(data, 0, 0)
-    print(data)
-    #print(data[:,2])
-
-    return np.ones((3,4))
-
-dataLoad('test.txt')
+    return data
 
 def dataStatistics(data, statistics):
     # Insert your code here
