b7d59bf4a0f23dd3532a8ee14cb95fc0805d2b25
Nicklas <s224218@dtu.dk>
2022-11-03 13:18:52 +0100

    DATA LOAD YAY

--- main.py
+++ main.py
@@ -11,12 +11,30 @@ def dataLoad(filename):
     ##########################
 
     #Converting file to Matrix
-    for row in filename:
-        data = []
-        data.append([int(x) for x in row.split()])
 
+    # for row in filename:
+    #     data = []
+    #     data.append([int(x) for x in row.split()])
 
+    tmp = np.loadtxt(filename, dtype=float)
+    data = np.zeros(3)
     print(data)
+    for n in range(tmp[:,0].size):
+        stack = False
+        if tmp[n,0] > 10 and tmp[n,0] < 60 and tmp[n,1] > 0 and tmp[n,2] >= 1 and tmp[n,2] <= 4:
+            stack = True
+            #data = np.vstack((data, tmp[n,:]))
+        else:
+            print("Invalid data on line " + str(n + 1) + ": Not in range between 10-60 ;_;"
+
+        if tmp[n,1] > 0:
+            stack = True
+        else:
+            print("Invalid data on line " + str(n + 1) + ": Not a positive number :(")
+
+    data = np.delete(data, 0, 0)
+    print(data)
+    #print(data[:,2])
 
     return np.ones((3,4))
 
