9a17e17dae80172da75ef347def49d1861a11520
Nicklas <s224218@dtu.dk>
2022-11-04 12:52:37 +0100

    Some changes to main()

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 creditsShown = False
 credits = "created by Nicklas: s224218, Sophia: s224222, Jonas: s224191"
+filenameChoice = ""
+dataChoice = ""
+
 
 def dataLoad(filename):
     ######################################################
@@ -53,19 +56,19 @@ def dataStatistics(data, statistics):
     coldGrowth = np.mean(coldData)
     hotGrowth = np.mean(hotData)
 
-    if (statistics).lower == "mean temperature":
+    if (statistics).lower == "mean temperature" or statistics == "1":
         return meanTemp
-    elif (statistics).lower == "mean growth rate":
+    elif (statistics).lower == "mean growth rate" or statistics == "2":
         return meanGrowth
-    elif (statistics).lower == "std temperature":
+    elif (statistics).lower == "std temperature" or statistics == "3":
         return stdTemp
-    elif (statistics).lower == "std growth rate":
+    elif (statistics).lower == "std growth rate" or statistics == "4":
         return stdGrowth
-    elif (statistics).lower == "rows":
+    elif (statistics).lower == "rows" or statistics == "5":
         return rows
-    elif (statistics).lower == "mean cold growth rate":
+    elif (statistics).lower == "mean cold growth rate" or statistics == "6":
         return coldGrowth
-    elif (statistics).lower == "mean hot growth rate":
+    elif (statistics).lower == "mean hot growth rate" or statistics == "7":
         return hotGrowth
     return
 
@@ -93,21 +96,49 @@ def main():
     print("4. Generate diagrams/plot data\n")
     print("5. Exit program\n")
     choice = input("Your choice: ")
+
     if choice == "1":
         print("\nYou chose to load data from file")
         print("Please write the name of the file you want to load data from.\nYou can write 'exit', if you want to go back to the menu\n")
         while True:
             try:
-                filename = input("Filename: ")
-                if filename == "exit":
+                filenameChoice = input("Filename: ")
+                if filenameChoice == "exit":
                     main()
-                data = dataLoad(filename)
+                data = dataLoad(filenameChoice)
                 print("Data loaded successfully! :D\nYou will now be redirected to the menu\n")
                 main()
                 break
             except OSError:
                 print("Invalid filename, please try again")
-    #elif choice == "3":
+    
+    elif choice == "2":
+        print(filenameChoice)
+
+    
+    elif choice == "3":
+        while True:
+            print("\nYou chose to show statistics. (You can write either the specific number or the specific text to get the statistic)\n")
+            print("These are the options you can choose from:\n")
+            print("1. Mean temperature\n")
+            print("2. Mean growth rate\n")
+            print("3. Standard deviation of temperature\n")
+            print("4. Standard deviation of growth rate\n")
+            print("5. Number of rows\n")
+            print("6. Mean cold growth rate\n")
+            print("7. Mean hot growth rate\n")
+            print("8. Exit program\n")
+            try:
+                statisticChoice = input("Your choice: ")
+                if (statisticChoice).lower == "exit program" or statisticChoice == "8":
+                    main()
+                print(dataStatistics(data, statisticChoice))
+                # print("Data loaded successfully! :D\nYou will now be redirected to the menu\n")
+                # main()
+                break
+            except OSError:
+                print("Invalid number/text :( please try again")
+
 
 
 
